9d63ceb60bd2276662fc223006efc1baf72b7413
Alexander Smirnov <panthernet@mail.ru>
2021-02-11 23:30:57 +0100

    Added some sockets barebone Added version

---
+++ THDWebServer/Version.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+
+namespace THDWebServer
+{
+    public partial class Program
+    {
+        public const string VERSION = "0.1.0";
+    }
+}
+
+
