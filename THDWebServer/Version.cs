54f0235c90d8d917dffc3e882818b2d206e78b14
Alexander Smirnov <panthernet@mail.ru>
2021-02-13 23:13:36 +0100

    + Fixed timers auth update

--- THDWebServer/Version.cs
+++ THDWebServer/Version.cs
@@ -1,13 +1,13 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
 
 namespace THDWebServer
 {
     public partial class Program
     {
-        public const string VERSION = "0.1.0";
+        public const string VERSION = "0.2.0";
     }
 }
 
